e1fc03e99cc7253647e340a8f6bd8dccf431d474
Anugrah K <anugrahkorembeth.ak@gmail.com>
2023-06-22 17:08:45 +0530

    implemented base events

--- lib/screens/home/bloc/home_bloc.dart
+++ lib/screens/home/bloc/home_bloc.dart
@@ -3,16 +3,20 @@ import 'dart:async';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter_application_bloc/data/grocery_data.dart';
+
+import '../../../models/grocery_model.dart';
 
 part 'home_event.dart';
 part 'home_state.dart';
 
 class HomeBloc extends Bloc<HomeEvent, HomeState> {
-  HomeBloc() : super(HomeInitial()) {
+  HomeBloc() : super(HomeInitialState()) {
+    on<HomeInitialEvent>(homeInitialEvent);
     on<WishlistButtonPressedEvent>(wishlistButtonPressedEvent);
     on<CartButtonClickedEvent>(cartButtonClickedEvent);
     on<NavigateToWishListPageEvent>(navigateToWishListEvent);
-    on<NavigateToCartPageEvent>(navigateToCartPageState);
+    on<NavigateToCartPageEvent>(navigateToCartPageEvent);
   }
 
   FutureOr<void> wishlistButtonPressedEvent(
@@ -31,15 +35,32 @@ class HomeBloc extends Bloc<HomeEvent, HomeState> {
 
   FutureOr<void> navigateToWishListEvent(
       NavigateToWishListPageEvent event, Emitter<HomeState> emit) {
-    if (kDebugMode) {
-      print('Navigated to wishlist');
-    }
+    // if (kDebugMode) {
+    //   print('Navigated to wishlist');
+    // }
+    emit(NavigateToWishlistPageActionState());
   }
 
-  FutureOr<void> navigateToCartPageState(
+  FutureOr<void> navigateToCartPageEvent(
       NavigateToCartPageEvent event, Emitter<HomeState> emit) {
-        if (kDebugMode) {
-      print('Navigated to cart');
-    }
-      }
+    // if (kDebugMode) {
+    //   print('Navigated to cart');
+    // }
+    emit(NavigateToCartActionPageState());
+  }
+
+  FutureOr<void> homeInitialEvent(
+      HomeInitialEvent event, Emitter<HomeState> emit) async {
+    emit(HomeLoadingState());
+    await Future.delayed(const Duration(seconds: 1));
+    emit(HomeLoadedState(
+        groceryItems: GroceryData.groceryProducts
+            .map((e) => GroceryModel(
+                id: e['id'],
+                name: e['name'],
+                price: e['price'],
+                quantity: e['quantity'],
+                category: e['category']))
+            .toList()));
+  }
 }
