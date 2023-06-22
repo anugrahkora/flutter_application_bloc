d8c226b8c4e42deca1b523238016765d7659c99c
Anugrah K <anugrahkorembeth.ak@gmail.com>
2023-06-22 17:08:23 +0530

    added grocery Model

---
+++ lib/models/grocery_model.dart
@@ -0,0 +1,10 @@
+class GroceryModel {
+  final int id;
+  final String name;
+  final double price;
+  final int quantity;
+  final String category;
+
+  GroceryModel(
+      {required this.id,required this.name,required this.price,required this.quantity,required this.category});
+}
